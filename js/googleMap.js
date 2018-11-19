b1686c6fb5416cf6a77e4795840cc57d29d4154f
Simonas Petkevičius <simonas.petk@gmail.com>
2018-11-19 00:19:04 +0000

    Googlemaps info window created

--- js/googleMap.js
+++ js/googleMap.js
@@ -1,4 +1,8 @@
-// Initialize and add the map
+/* 
+	code for map is used from: https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple-max
+	The code is sligthly edited to fit our needs.
+	It is licensed over Apache License 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+	Initialize and add the map */
 function initMap() {
     // The location of Sheffield Student's union
     var studentsUnion = {lat: 53.38072, lng: -1.4871329999999716};
@@ -6,8 +10,24 @@ function initMap() {
     var map = new google.maps.Map(
     document.getElementById('map'), {zoom: 17, center: studentsUnion});
 
-    //add text and info about SU later google maps info window
+    //Picture added to SU google maps info window
+    var contentString = 
+            '<h1>Sheffield Student\'s Union</h1>'+
+            '<img src = "images/sumap.png">';
+
+    // create info window
+    var infowindow = new google.maps.InfoWindow({
+    	content: contentString,
+    	maxWidth: 350,
+    	minWidth: 300
+    });
 
     // The marker, positioned at Sheffield Student's union
-    var marker = new google.maps.Marker({position: studentsUnion, map: map});
+    var marker = new google.maps.Marker({position: studentsUnion, map: map, title: 'Sheffield Student\'s Union'});
+
+    // display SU InfoWindow when clicked
+    marker.addListener('click', function() {
+    	infowindow.open(map,marker);
+    });
+
 }
